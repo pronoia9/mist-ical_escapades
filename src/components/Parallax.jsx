ec819ef7024a9f1565a2f19ab64708a242648e66
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-02 21:59:39 +0300

    add init structure + add/map images

--- src/components/Parallax.jsx
+++ src/components/Parallax.jsx
@@ -1,3 +1,34 @@
+import styled from 'styled-components';
+
+import { parallaxImages } from '../assets';
+
 export default function Parallax() {
-  return <div>Parallax</div>;
+  return (
+    <Container>
+      {/* Behind the text */}
+      {parallaxImages.bottom.map(({ title, image, parallax }) => (
+        <Image key={title} className={`${parallax ? 'parallax ' : ''}${title}`} src={image} />
+      ))}
+
+      {/* Title */}
+      <TextContainer>
+        <h2>Mist-ical</h2>
+        <h1>Escapades</h1>
+      </TextContainer>
+
+      {/* On top of the title */}
+      {parallaxImages.top.map(({ title, image, parallax }) => (
+        <Image key={title} className={`${parallax ? 'parallax ' : ''}${title}`} src={image} />
+      ))}
+    </Container>
+  );
 }
+
+const Container = styled.main`
+  background-color: #353;
+  height: 100vh;
+`;
+
+const Image = styled.img``;
+
+const TextContainer = styled.div``;
