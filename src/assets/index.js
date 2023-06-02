af8340777436158cfa10af3a1abbf3f485db2666
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-02 17:46:23 +0300

    add images/assets

---
+++ src/assets/index.js
@@ -0,0 +1,45 @@
+import background from './background.png';
+import blackShadow from './black_shadow.png';
+import fog1 from './fog_1.png';
+import fog2 from './fog_2.png';
+import fog3 from './fog_3.png';
+import fog4 from './fog_4.png';
+import fog5 from './fog_5.png';
+import fog6 from './fog_6.png';
+import fog7 from './fog_7.png';
+import logo from './logo.png';
+import mountain1 from './mountain_1.png';
+import mountain2 from './mountain_2.png';
+import mountain3 from './mountain_3.png';
+import mountain4 from './mountain_4.png';
+import mountain5 from './mountain_5.png';
+import mountain6 from './mountain_6.png';
+import mountain7 from './mountain_7.png';
+import mountain8 from './mountain_8.png';
+import mountain9 from './mountain_9.png';
+import mountain10 from './mountain_10.png';
+import sunRays from './sun_rays.png';
+
+export {
+  background,
+  blackShadow,
+  fog1,
+  fog2,
+  fog3,
+  fog4,
+  fog5,
+  fog6,
+  fog7,
+  logo,
+  mountain1,
+  mountain2,
+  mountain3,
+  mountain4,
+  mountain5,
+  mountain6,
+  mountain7,
+  mountain8,
+  mountain9,
+  mountain10,
+  sunRays,
+};
