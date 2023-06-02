1437d30af25dc88c0cf458b3f61b1a98f28b2105
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-02 19:21:44 +0300

    add routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
+import { Routes, Route } from 'react-router-dom';
+
 import { Navbar } from './components';
+import { HomePage, ContactPage } from './pages';
 
 export default function App() {
   return (
     <>
       <Navbar />
+
+      <Routes>
+        <Route path='/contact' element={<ContactPage />} />
+        <Route exact path='/' element={<HomePage />} />
+      </Routes>
     </>
   );
 }
