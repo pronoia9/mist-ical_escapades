55d465510739443caecf46da72d756ebd47c7216
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-02 20:16:08 +0300

    comment out init/default colors

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,13 +1,13 @@
 export const light = {
-  primary: '#345995',
-  secondary: '#FF6B6B',
-  background: '#FFFFFF',
-  text: '#333333',
+  // primary: '#345995',
+  // secondary: '#FF6B6B',
+  // background: '#FFFFFF',
+  // text: '#333333',
 };
 
 export const dark = {
-  primary: '#7B2CBF',
-  secondary: '#FF6B6B',
-  background: '#1C1C1C',
-  text: '#FFFFFF',
+  // primary: '#7B2CBF',
+  // secondary: '#FF6B6B',
+  background: '#333333',
+  // text: '#FFFFFF',
 };
