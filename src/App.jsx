0753c6d2f2e7f1b6e394b5f9b2bba5c935617cd8
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-02 20:12:25 +0300

    add global styles + theme to app

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,36 @@
+import { useState, useEffect } from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 
 import { Navbar } from './components';
 import { HomePage, ContactPage } from './pages';
+import { GlobalStyles } from './styles/GlobalStyles';
+import { light, dark } from './styles/Theme';
 
 export default function App() {
+  // THEME RELATED THINGS
+  const [isDarkMode, setIsDarkMode] = useState(window.matchMedia('(prefers-color-scheme: dark)').matches);
+  const toggleTheme = () => { setIsDarkMode((prevMode) => !prevMode); };
+  // Listen for system theme change
+  useEffect(() => {
+    const handleChange = (event) => { setIsDarkMode(event.matches); };
+    const darkModeQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    darkModeQuery.addEventListener('change', handleChange);
+    return () => { darkModeQuery.removeEventListener('change', handleChange); };
+  }, []);
+
   return (
     <>
-      <Navbar />
+      <ThemeProvider theme={isDarkMode ? dark : light}>
+        <GlobalStyles />
+        <Navbar />
 
-      <Routes>
-        <Route path='/contact' element={<ContactPage />} />
-        <Route exact path='/' element={<HomePage />} />
-      </Routes>
+        <button onClick={toggleTheme}>Toggle Theme</button>
+        <Routes>
+          <Route path='/contact' element={<ContactPage />} />
+          <Route exact path='/' element={<HomePage />} />
+        </Routes>
+      </ThemeProvider>
     </>
   );
 }
