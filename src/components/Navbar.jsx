4c6b5352cc5bc66bb2caa28fd61e37f69ef0f13e
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-02 21:30:52 +0300

    finish init navbar + styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -14,7 +14,7 @@ export default function Navbar() {
     <Container>
       <Wrapper>
         {/* Logo */}
-        <img src={logo} />
+        <Logo src={logo} />
 
         {/* Navlinks */}
         <NavList>
@@ -25,13 +25,14 @@ export default function Navbar() {
           ))}
 
           {/* Search Icon */}
-          <NavListItem>
+          <NavListItem className='search'>
             <i className='fa-sharp fa-solid fa-magnifying-glass' />
           </NavListItem>
 
           {/* Hamburger Icon */}
-          <NavListItem>
+          <NavListItem className='hamburger'>
             <HamburgerWrapper>
+              <HamburgerBar />
             </HamburgerWrapper>
           </NavListItem>
         </NavList>
@@ -40,12 +41,85 @@ export default function Navbar() {
   );
 }
 
-const Container = styled.header``;
+const Container = styled.header`
+  width: 100%;
+  border-bottom: 1px solid rgba(255, 255, 255, 0.1);
+`;
 
-const Wrapper = styled.nav``;
+const Wrapper = styled.nav`
+  /* max-width: 1400px; */
+  padding: 0 2rem;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
 
-const NavList = styled.ul``;
+const Logo = styled.img`
+  width: 100px;
+`;
 
-const NavListItem = styled.li``;
+const NavList = styled.ul`
+  display: flex;
+  align-items: center;
+`;
 
-const HamburgerWrapper = styled.div``;
+const NavListItem = styled.li`
+  a {
+    color: #fff;
+    padding: 0 1.5rem;
+    text-transform: uppercase;
+    font-weight: 300;
+    font-size: 0.83rem;
+  }
+
+  &.search i {
+    font-size: 1.05rem;
+    padding: 0 3rem;
+    cursor: pointer;
+  }
+
+  &.hamburger {
+    padding-left: 1.5rem;
+  }
+`;
+
+const HamburgerWrapper = styled.div`
+  padding: 0;
+  width: 37px;
+  height: 37px;
+  border-radius: 50%;
+  background-color: rgba(155, 155, 155, 0.7);
+  backdrop-filter: blur(10px);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const HamburgerBar = styled.div`
+  position: relative;
+  width: 52%;
+  height: 1.3px;
+  background-color: #fff;
+  border-radius: 2px;
+
+  &::before,
+  &::after {
+    content: '';
+    position: absolute;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 60%;
+    height: 100%;
+    background-color: inherit;
+    border-radius: 2px;
+  }
+
+  &::before {
+    top: -4.5px;
+  }
+
+  &::after {
+    top: 4.5px;
+  }
+`;
