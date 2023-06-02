9d0c38a87bffe1ec0824a379befa90a226808770
pronoia9 <19336274+pronoia9@users.noreply.github.com>
2023-06-02 20:13:21 +0300

    add init theme

---
+++ src/styles/Theme.js
@@ -0,0 +1,13 @@
+export const light = {
+  primary: '#345995',
+  secondary: '#FF6B6B',
+  background: '#FFFFFF',
+  text: '#333333',
+};
+
+export const dark = {
+  primary: '#7B2CBF',
+  secondary: '#FF6B6B',
+  background: '#1C1C1C',
+  text: '#FFFFFF',
+};
